f01c706f406d280598ddbf19e6610038de82651f
Nishu singh <nishu.frontend@gmail.com>
2026-04-29 13:09:03 +0530

    learn about installation, nodejs need,runnig nodejs code

---
+++ Section-1/03-why-need-nodejs/script.js
@@ -0,0 +1,17 @@
+const { exec } = require("child_process");
+exec("start whatsapp");
+
+// const fs = require("fs");
+// fs.unlinkSync("C:\\Users\\admin\\Desktop\\hello.txt");
+// fs.renameSync(
+//   "C:\\Users\\admin\\Desktop\\text.txt",
+//   "C:\\Users\\admin\\Desktop\\hello.txt",
+// );
+// fs.writeFileSync(
+//   "C:\\Users\\admin\\Desktop\\text.txt",
+//   "Hello Nishu singh, Heyyyy Dude!!!",
+// );
+// const text = fs.readFileSync("C:\\Users\\admin\\Desktop\\text.txt");
+
+// console.log(text.toString());
+// console.log("End");

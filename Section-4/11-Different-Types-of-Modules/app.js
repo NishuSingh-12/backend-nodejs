a3db949e5ccc676c14edb20f85ffba866d5bf50d
Nishu singh <nishu.frontend@gmail.com>
2026-04-29 13:39:21 +0530

    learn about different type of modules

---
+++ Section-4/11-Different-Types-of-Modules/app.js
@@ -0,0 +1,12 @@
+// Native Module or Core Module
+import fs from "node:fs";
+import net from "node:net";
+import dgram from "node:dgram";
+import https from "node:https";
+import http from "node:http";
+
+// User Module
+import { num } from "./math";
+
+// NPM Module
+import axios from "axios";

a8debf79b5e81da3716a6406940137a1970888a0
Nishu singh <nishu.frontend@gmail.com>
2026-04-29 13:17:06 +0530

    Learn about threads

---
+++ Section-3/01-threads/app.js
@@ -0,0 +1,4 @@
+const { Worker } = require("worker_threads");
+new Worker("./a.js");
+new Worker("./b.js");
+new Worker("./c.js");
---
+++ Section-3/01-threads/c.js
@@ -0,0 +1,6 @@
+console.log("C file started");
+for (let i = 0; i < 1000000000; i++) {
+  if (i % 400000000 == 0) {
+    console.log(`Running loop one 3 ${i}`);
+  }
+}

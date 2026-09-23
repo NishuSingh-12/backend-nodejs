2615b40b6e25377a608fd40f871c0683572ff2e7
Nishu singh <nishu.frontend@gmail.com>
2026-09-23 15:36:45 +0530

    AWS S3 SDK Setup in Node.js

---
+++ Section-18(Cloud Storage Integration with AWS S3)/20-AWS-S3-SDK-Setup-in-Node.js/app.js
@@ -0,0 +1,10 @@
+// s3Client.js
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({
+  profile: "nodejs",
+});
+
+const command = new ListBucketsCommand();
+const response = await s3Client.send(command);
+console.log(response.Buckets);

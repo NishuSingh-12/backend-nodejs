e577b6aff2c2a65d2811cb6b78c06be78ac36a47
Nishu singh <nishu.frontend@gmail.com>
2026-09-25 22:11:06 +0530

    Integrating S3 Bucket – File Uploads

---
+++ Section-18(Cloud Storage Integration with AWS S3)/32-Integrating-S3-Bucket - File-Uploads/server/routes/fileRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import validateIdMiddleware from "../middlewares/validateIdMiddleware.js";
+import {
+  deleteFile,
+  getFile,
+  renameFile,
+  uploadInitiate,
+} from "../controllers/fileController.js";
+
+const router = express.Router();
+
+router.param("parentDirId", validateIdMiddleware);
+router.param("id", validateIdMiddleware);
+
+router.post("/upload/initiate", uploadInitiate);
+
+// router.post("/:parentDirId?", uploadFile);
+
+router.get("/:id", getFile);
+
+router.patch("/:id", renameFile);
+
+router.delete("/:id", deleteFile);
+
+export default router;

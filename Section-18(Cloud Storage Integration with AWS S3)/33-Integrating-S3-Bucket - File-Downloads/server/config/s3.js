9e514b3d04d6b8b2f854439298f9b7a83ec125f9
Nishu singh <nishu.frontend@gmail.com>
2026-09-26 17:30:15 +0530

    Integrating S3 Bucket File Download

---
+++ Section-18(Cloud Storage Integration with AWS S3)/33-Integrating-S3-Bucket - File-Downloads/server/config/s3.js
@@ -0,0 +1,39 @@
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+export const s3Client = new S3Client({ profile: "nodejs" });
+
+export const createUploadSignedUrl = async ({ key, contentType }) => {
+  const command = new PutObjectCommand({
+    Bucket: "coderr-storage-app",
+    Key: key,
+    ContentType: contentType,
+  });
+
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: 300,
+    signableHeaders: new Set(["content-type"]),
+  });
+  return url;
+};
+
+export const createGetSignedUrl = async ({
+  key,
+  download = false,
+  filename,
+}) => {
+  const command = new GetObjectCommand({
+    Bucket: "coderr-storage-app",
+    Key: key,
+    ResponseContentDisposition: `${download ? "attachment" : "inline"}; filename=${encodeURIComponent(filename)}`,
+  });
+
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: 300,
+  });
+  return url;
+};
---
+++ Section-18(Cloud Storage Integration with AWS S3)/33-Integrating-S3-Bucket - File-Downloads/server/routes/fileRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import validateIdMiddleware from "../middlewares/validateIdMiddleware.js";
+import {
+  deleteFile,
+  getFile,
+  renameFile,
+  uploadInitiate,
+} from "../controllers/fileController.js";
+
+const router = express.Router();
+
+router.param("parentDirId", validateIdMiddleware);
+router.param("id", validateIdMiddleware);
+
+router.post("/upload/initiate", uploadInitiate);
+
+router.get("/:id", getFile);
+
+router.patch("/:id", renameFile);
+
+router.delete("/:id", deleteFile);
+
+export default router;
